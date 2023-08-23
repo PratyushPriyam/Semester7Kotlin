e2dd4518f7a81d133e9e3f70021d3fc3c855e923
praty <pratyushpriyam49@gmail.com>
2023-08-23 12:58:19 +0530

    Bound Service Done.

---
+++ app/src/main/java/com/project/semester7cw/Services/BoundService/BoundServiceHelperClass.kt
@@ -0,0 +1,44 @@
+package com.project.semester7cw.Services.BoundService
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.project.semester7cw.R
+
+class BoundServiceHelperClass: Service() {
+    val channel_id = "Foreground Service Using Kotlin"
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val input = intent?.getStringExtra("inputExtra")
+        createNotificationChannel()
+        val notificationIntent = Intent(this, BoundServiceHelperClass::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        val notification = NotificationCompat.Builder(this, channel_id)
+            .setContentTitle("Foreground Service")
+            .setContentText(input)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(1, notification)
+        return START_NOT_STICKY
+//        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(channel_id, "Foreground Service Channel", NotificationManager.IMPORTANCE_HIGH)
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+
+        }
+
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
---
+++ app/src/main/java/com/project/semester7cw/Services/BoundService/BoundServiceMain.kt
@@ -0,0 +1,30 @@
+package com.project.semester7cw.Services.BoundService
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import com.project.semester7cw.R
+
+class BoundServiceMain : AppCompatActivity() {
+    lateinit var startBoundBtn: Button
+    lateinit var stopBoundBtn: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bound_service_main)
+        startBoundBtn = findViewById(R.id.startBoundService)
+        stopBoundBtn = findViewById(R.id.stopBoundService)
+
+        startBoundBtn.setOnClickListener {
+            val intent = Intent(this, BoundServiceHelperClass::class.java)
+            intent.putExtra("inputExtra", "Foreground Service is running")
+            ContextCompat.startForegroundService(this, intent)
+        }
+
+        stopBoundBtn.setOnClickListener {
+            val intent = Intent(this, BoundServiceHelperClass::class.java)
+            stopService(intent)
+        }
+    }
+}
